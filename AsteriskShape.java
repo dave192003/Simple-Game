5d47f40cc83926f5256411bc11ce67bd9393d375
Dev Lebico <lebicodavedarylpajares09@gmail.com>
2025-07-01 19:49:03 +0800

    I added two class for printing shapes,I also fix some errors.

--- AsteriskShape.java
+++ AsteriskShape.java
@@ -3,19 +3,21 @@ public class AsteriskShape{
   public static void main(String[] args){
       Print Print=new Print();
       PrintPyramid PrintPyramid = new PrintPyramid();
+      PrintRRT PrintRRT=new PrintRRT();
+      PrintDiamond PDiamond=new PrintDiamond();
       Scanner scanner =new Scanner(System.in);
       System.out.print("Print Shapes using asterisk\n");
       System.out.println("_________________________________________");
       boolean islooping=true;
       
       while (islooping){
-        System.out.print("Choose shape you to print using asterisk;\n1).Right Triangle/n2).Reversed Right Triangle\n3).Diamond\n4).Pyramid\n5).Square\n6).Rectangle\nYour choice: ");
+        System.out.print("Choose shape you to print using asterisk;\n1).Right Triangle\n2).Reversed Right Triangle\n3).Diamond\n4).Pyramid\n5).Square\n6).Rectangle\nYour choice: ");
         int UserChoice=scanner.nextInt();
       
         switch(UserChoice){
           case 1-> Print.triangle();
-          case 2-> System.out.print("Hahahah");
-          case 3-> System.out.print("Gagagaga");
+          case 2-> PrintRRT.RRT();
+          case 3-> PDiamond.diamond();
           case 4-> PrintPyramid.pyramid();
           default -> System.out.print("Invalid input");
         }
@@ -64,3 +66,41 @@ class PrintPyramid{
         }
     }
 }            
+class PrintRRT{
+  void RRT(){
+    int rows=8;
+    for (int i=0;i<=rows;i++){
+      for (int j=0;j<=rows-1;j++){
+        System.out.print(" ");
+      }
+      for (int k=0;k<=(rows-rows)+i;k++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
+
+class PrintDiamond{
+  void diamond(){
+    int rows=8;
+    for (int i=0;i<=rows;i++){
+      for(int j=0;j<=rows-i;j++){
+        System.out.print(" ");
+      }
+      for (int k=0;k<=(2*i)+1;k++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    for (int a=0;a<=rows;a++){
+        for (int b=0;b<=(rows-rows)+a;b++){
+            System.out.print(" ");
+        }
+        for (int c=0;c<=(rows-a)*2;c++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+  }
+}
