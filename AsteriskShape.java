c127427a46fe4c69c6e2a4c59373a72ed8c934e6
Dev Lebico <lebicodavedarylpajares09@gmail.com>
2025-07-01 15:55:48 +0800

    I change the class named Main into AsteriskShape

--- AsteriskShape.java
+++ AsteriskShape.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main{
+public class AsteriskShape{
   public static void main(String[] args){
       Print Print=new Print();
       PrintPyramid PrintPyramid = new PrintPyramid();
