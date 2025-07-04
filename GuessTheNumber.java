98a23a52329b7080b1ff2dda9257d9a5b6caf70d
Dev Lebico <lebicodavedarylpajares09@gmail.com>
2025-07-04 22:40:52 +0800

    Enhanced game

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -6,9 +6,11 @@ public class GuessTheNumber{
   Scanner scanner=new Scanner(System.in);
   int lives=10;
   int RandomNum= RandomNumber.nextInt(100);
+  int CorrectGuess=0;
   boolean islooping=true;
   String Difficulty= "Easy";
   String Range= " 1 to 100";
+  
   
   public static void main(String[] args){
     GuessTheNumber Game=new GuessTheNumber();
@@ -25,9 +27,9 @@ public class GuessTheNumber{
   
   
   public void GameLoop(){
-    
+      
     System.out.println("Guess the hidden number between" +Range+ "."+ "GOOD LUCK!\n");
-    System.out.println("Difficulty= "+ Difficulty);
+    System.out.print("||||||||||| Difficulty= "+ Difficulty+ " ||||||||||||\n");
     while (islooping){
       
       System.out.println("Remaining Life: "+lives);
@@ -36,24 +38,44 @@ public class GuessTheNumber{
       scanner.nextLine();
       if (guess< RandomNum){
         System.out.println("Higher!");
-        lives=lives-1;
+        lives--;
         
       }
+    
       else if (guess >RandomNum){
         System.out.println("Lower!");
-        lives=lives-1;
+        lives--;
       }
+      
       
       else{
         System.out.println("Correct!, Well guess!");
+        
+        CorrectGuess++;
         Difficulty="Hard";
-        RandomNum=RandomNumber.nextInt(500);
+        RandomNum=RandomNumber.nextInt(1000);
         lives=10;
-        Range="1 to 500";
-        System.out.println("----------------LEVEL UP----------------");
-        System.out.println("Difficulty= "+ Difficulty);
-        System.out.println("Guess the hidden number between " +Range+ ". "+ "GOOD LUCK!\n");
+        Range="1 to 1000";
+        System.out.println("\n----------------LEVEL UP----------------");
+        
       }
+      if (CorrectGuess==1&&lives==10){
+        System.out.print("||||||||||| Difficulty= "+ Difficulty+ " ||||||||||||\n");
+        System.out.println("Guess the hidden number between " +Range+ ". "+ "GOOD LUCK!\n");
+        
+      }
+      
+      if (CorrectGuess==2&&lives==10){
+        
+        RandomNum=RandomNumber.nextInt(100000);
+        Range="(unknown)";
+        Difficulty= "IMPOSSIBLE";
+        
+        System.out.print("|||||||| Difficulty= "+ Difficulty+ " ||||||||\n");
+        System.out.println("Guess the hidden number between " +Range+ ". "+ "GOOD LUCK!\n");
+        System.out.println("You are entering the hardest level, in this level, I will not to tell you the range of numbers. So the number that will generate is so random that possible it reach to a 100,000.\nIf you correct the right number, the developer will give you a money as a price. GOOD LUCK!.");
+      }
+      
       if (lives<=0){
         islooping=false;
         System.out.println("You ran out of life.");
